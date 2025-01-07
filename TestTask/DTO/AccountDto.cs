b8005313d7989443a0e9e2c1b048cdf34b2d2e9d
Vitaliy <vitaliy.yanitskyy@gmail.com>
2025-01-07 20:41:33 +0200

    Add DTOs; add ContactsController(tested)

---
+++ TestTask/DTO/AccountDto.cs
@@ -0,0 +1,7 @@
+﻿namespace TestTask.DTO;
+
+public class AccountDto
+{
+    public string Name { get; set; }
+    public string ContactEmail { get; set; }
+}
---
+++ TestTask/DTO/IncidentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TestTask.DTO;
+
+public class IncidentDto
+{
+    public string AccountName { get; set; }
+    public string ContactFirstName { get; set; }
+    public string ContactLastName { get; set; }
+    public string ContactEmail { get; set; }
+    public string Description { get; set; }
+}
