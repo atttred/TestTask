50c0e1843b4c54a178a1efeaa2c773120d17951d
Vitaliy <vitaliy.yanitskyy@gmail.com>
2025-01-07 18:17:12 +0200

    Create DbContext

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -1,11 +1,13 @@
+using TestTask.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
 
