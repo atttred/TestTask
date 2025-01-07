9b087328a9d9ef7b2888ade2be90f8aa7bcb324f
Vitaliy <vitaliy.yanitskyy@gmail.com>
2025-01-07 18:00:37 +0200

    Create models

---
+++ TestTask/Models/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace TestTask.Models;
+
+public class Account
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int IncidentId { get; set; }
+
+    public Incident Incident { get; set; }
+    public ICollection<Contact> Contacts { get; set; }
+}
---
+++ TestTask/Models/Incident.cs
@@ -0,0 +1,10 @@
+﻿namespace TestTask.Models;
+
+public class Incident
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public ICollection<Account> Accounts { get; set; }
+}
